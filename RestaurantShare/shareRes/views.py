b113f18e91f3f6a332fb4a33d897e4b591d0772a
anio <kitbung0214@gmail.com>
2023-07-30 18:16:01 +0900

    Edu-RSD, html fixxed

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
+def index(request):
+
+    return render(request, 'shareRes/index.html')
+    
+def restaurantDetail(request,res_id):
+    return render(request, 'shareRes/restaurantDetail.html')
+    
+def restaurantCreate(request):
+    return render(request, 'shareRes/restaurantCreate.html')
+
+def categoryCreate(request):
+
+    return render(request, 'shareRes/categoryCreate.html')
